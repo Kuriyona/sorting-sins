4eaa38f3465a0a15b6ad16dfb800386e7ecf5ad1
Kuriyona <Kuriyona@outlook.com>
2026-07-14 23:48:01 +0800

    fix: use real non-deterministic comparator with cmp_to_key in Python

--- python/main.py
+++ python/main.py
@@ -1,7 +1,8 @@
 import random
+from functools import cmp_to_key
 
 arr = list(range(100))
 
-arr.sort(key=lambda x: random.random())
+arr.sort(key=cmp_to_key(lambda a, b: random.choice([-1, 0, 1])))
 
 print(arr)
